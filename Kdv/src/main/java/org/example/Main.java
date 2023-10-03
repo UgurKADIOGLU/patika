1d8427b85d86938c2a1e60a8900352bd2c8ec26e
UgurKADIOGLU <ugrkadioglu@gmail.com>
2023-10-03 19:55:37 +0300

    Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan program

--- Kdv/src/main/java/org/example/Main.java
+++ Kdv/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("kdv'sini hesaplamak istediğiniz rakamı yazınız : ");
+        double deger= scanner.nextInt();
+        double tutar=(deger<1000)?deger*1.18:deger*1.08;
+        System.out.println(tutar);
     }
 }
