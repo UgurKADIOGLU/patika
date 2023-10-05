75420cc95e5401888a92dfce6baf24c500e2fc2b
UgurKADIOGLU <ugrkadioglu@gmail.com>
2023-10-05 14:47:48 +0300

    Hesap Makinesi switch case

---
+++ kosulluIfadeler/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   int numberOne, numberTwo,secim,sonuc;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Birinci sayıyı girniz : ");
+
+        numberOne=scanner.nextInt();
+        System.out.println("İkinci sayıyı girniz : ");
+        numberTwo=scanner.nextInt();
+
+        System.out.println("1 Toplama\n 2 Çıkarma\n 3 Çarpma\n 4 Bölme\n");
+        System.out.println("Bir değer giriniz : ");
+        secim=scanner.nextInt();
+        /*if(secim==1){
+            System.out.println("Toplama işlemnin sonucu : "+(numberOne+numberTwo));
+        } else if (secim ==2) {
+            System.out.println("Çıkarma işlemnin sonucu : "+(numberOne-numberTwo));
+        }else if (secim ==3) {
+            System.out.println("Çarpma işlemnin sonucu : "+(numberOne*numberTwo));
+        }else if (secim ==4) {
+            System.out.println("Bölme işlemnin sonucu : "+(numberOne/numberTwo));
+        }*/
+        switch (secim){
+            case 1:
+                System.out.println("Toplama işlemnin sonucu : "+(numberOne+numberTwo));
+                break;
+            case 2:
+                System.out.println("Çıkarma işlemnin sonucu : "+(numberOne-numberTwo));
+                break;
+            case 3:
+                System.out.println("Çarpma işlemnin sonucu : "+(numberOne*numberTwo));
+                break;
+            case 4:
+                System.out.println("Bölme işlemnin sonucu : "+(numberOne/numberTwo));
+                break;
+        }
+
+
+    }
+}
