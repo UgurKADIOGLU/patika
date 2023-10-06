16e5bc655b9ddc8f8c0184e405010ee9edb45450
UgurKADIOGLU <ugrkadioglu@gmail.com>
2023-10-06 12:26:52 +0300

    Çin Zodyağı

---
+++ cinZodyagi1/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int dogumYili;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Doğum yılınızı giriniz : ");
+        dogumYili=scanner.nextInt();
+        switch (dogumYili%12){
+            case 0:
+                System.out.println("Maymun burcusunuz.");
+                break;
+            case 1:
+                System.out.println("Horoz burcusunuz.");
+                break;
+            case 2:
+                System.out.println("Köpek burcusunuz.");
+                break;
+            case 3:
+                System.out.println("Domuz burcusunuz.");
+                break;
+            case 4:
+                System.out.println("Fare burcusunuz.");
+                break;
+            case 5:
+                System.out.println("Öküz burcusunuz.");
+                break;
+            case 6:
+                System.out.println("Kaplan burcusunuz.");
+                break;
+            case 7:
+                System.out.println("Tavşan burcusunuz.");
+                break;
+            case 8:
+                System.out.println("Ejderha burcusunuz.");
+                break;
+            case 9:
+                System.out.println("Yılan burcusunuz.");
+                break;
+            case 10:
+                System.out.println("At burcusunuz.");
+                break;
+            case 11:
+                System.out.println("Koyun burcusunuz.");
+                break;
+        }
+    }
+}
