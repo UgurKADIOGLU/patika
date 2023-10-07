821511f0b4f143bd666a1dfbca8ffd0460d5961f
UgurKADIOGLU <ugrkadioglu@gmail.com>
2023-10-07 19:58:39 +0300

    elmas

---
+++ yildiz/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int deger=0;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Satır sayısını giriniz : ");
+        int n=scanner.nextInt();
+        for(int i=1;i<=n;i++){
+
+            for (int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=(2*i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<n;i++){
+            for (int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=(2*n)-2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
